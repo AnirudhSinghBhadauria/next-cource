f998220e7d2db0460d0e9863a4040e75fba95a36
Anirudh Singh Bhadauria <108342411+AnirudhSinghBhadauria@users.noreply.github.com>
2023-02-16 03:14:11 +0530

    preRendering section done

--- pages/last-sales.jsx
+++ pages/last-sales.jsx
@@ -1,15 +1,64 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from "react";
+import useSWR from "swr";
 
-const LastSales = () => {
-     useEffect(async ()=>{
-          const response = await fetch('https://console.firebase.google.com/u/0/project/next-js-course-8aa6a/database/next-js-course-8aa6a-default-rtdb/data/~2F');
-     },[])
+const LastSales = (sale) => {
+  const fetcher = (...args) => fetch(...args).then((res) => res.json());
+  const [sales, setSales] = useState(sale.sales);
+
+  const { data, isLoading } = useSWR(
+    "https://next-js-course-8aa6a-default-rtdb.firebaseio.com/sales.json",
+    fetcher
+  );
+
+  useEffect(() => {
+    if (data) {
+      const transformSales = [];
+
+      for (const key in data) {
+        transformSales.push({
+          id: key,
+          username: data[key].username,
+          volume: data[key].volume,
+        });
+      }
+
+      setSales(transformSales);
+    }
+  }, [data]);
+
+  if (isLoading) return <p>loading...</p>;
 
   return (
     <ul>
-
+      {sales.map((sales) => (
+        <li key={sales.id}>
+          {sales.username}-{sales.volume}
+        </li>
+      ))}
     </ul>
-  )
-}
+  );
+};
 
-export default LastSales
+export default LastSales;
+
+export async function getStaticProps() {
+  const response = await fetch(
+    "https://next-js-course-8aa6a-default-rtdb.firebaseio.com/sales.json"
+  );
+  const fSales = await response.json();
+
+  const transformSales = [];
+
+  for (const key in fSales) {
+    transformSales.push({
+      id: key,
+      username: fSales[key].username,
+      volume: fSales[key].volume,
+    });
+  }
+
+  return {
+    props: { sales: transformSales },
+    revalidate: 10,
+  };
+}
