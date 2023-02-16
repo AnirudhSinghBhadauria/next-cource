2ad2f0e467d4d4216c624f2d55656c991e435c28
Anirudh Singh Bhadauria <108342411+AnirudhSinghBhadauria@users.noreply.github.com>
2023-02-16 15:12:33 +0530

    Project-2 Starting

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,29 +1,11 @@
 import React from "react";
-import path from "path";
-import fs from "fs/promises";
-import Link from 'next/link'
 
-const HomePage = (props) => {
-  const {loadedProducts} = props;
+const HomePage = () => {
   return (
-    <ul>
-      {loadedProducts.map(({ id, title }) => {
-        return <Link href={`/product/${id}`} key={id}>{title}</Link>;
-      })}
-    </ul>
+    <div></div>
   );
 };
 
 export default HomePage;
 
-export async function getStaticProps() {
-  const filePath = path.join(process.cwd(), "data", "dummy-backend.json");
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
-  return {
-    props: {
-      loadedProducts: data.products,
-    },
-    revalidate: 10,
-  };
-};
+
