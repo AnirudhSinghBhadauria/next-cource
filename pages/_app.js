c1f3c4b2810232ef0fc66ef2df704a3a568444f2
Anirudh Singh Bhadauria <108342411+AnirudhSinghBhadauria@users.noreply.github.com>
2023-02-17 23:48:09 +0530

    api routes

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,7 @@
-import Layout from "../components/layout/layout";
 import "../styles/globals.css";
-import HeadContent from "@/components/Head/HeadContent";
-import { Fragment } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return (
-    <Fragment>
-      <HeadContent
-        title="NEXT | Events"
-        name="viewport"
-        content="width=device-width, initial-scale=1"
-      />
-
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Fragment>
-  );
+  return <Component {...pageProps} />;
 }
 
 export default MyApp;
--- pages/_document.js
+++ pages/_document.js
@@ -5,7 +5,6 @@ export default function Document() {
     <Html lang="en">
       <Head />
       <body>
-        <div id='overlay'></div>
         <Main />
         <NextScript />
       </body>
